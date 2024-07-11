4429d7ee16fb4cbc7291b39b18806cff897667bf
milenamuch <milenamuchenski@gmail.com>
2024-07-11 11:14:48 -0300

    mandando exercicio 3

---
+++ conceitos_basicos/3_conversao_medidas.py
@@ -0,0 +1,14 @@
+"""
+3. Faça um Programa que peça a quantidade de quilômetros, transforme em metros, centímetros e milímetros
+"""
+
+quilometros = float(input("Digite a quantidade de quilômetros: "))
+
+metros = quilometros * 1000
+centimetros = quilometros * 100000
+milimetros = quilometros * 1000000
+
+print(f"{quilometros} quilômetros é igual a:")
+print(f"{quilometros}km, é igual a {metros}m")
+print(f"{quilometros}km, é igual a {centimetros}cm")
+print(f"{quilometros}km, é igual a {milimetros}mm")
