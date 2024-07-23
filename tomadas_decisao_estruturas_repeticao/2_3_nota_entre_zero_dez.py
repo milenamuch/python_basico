94a695b12e61cba17b7bc95cdd2a6c35e80d8a73
milenamuch <milenamuchenski@gmail.com>
2024-07-23 15:37:58 -0300

    inserindo o enunciado do exercicio

--- tomadas_decisao_estruturas_repeticao/2_3_nota_entre_zero_dez.py
+++ tomadas_decisao_estruturas_repeticao/2_3_nota_entre_zero_dez.py
@@ -1,3 +1,9 @@
+"""
+3. Faça um programa que peça uma nota, entre zero e dez. Mostre uma
+mensagem caso o valor seja inválido e continue pedindo até que o usuário
+informe um valor válido.
+"""
+
 try:
 
     while True:
